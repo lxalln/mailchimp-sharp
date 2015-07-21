36923e33e8fb637332f5aef9674c129e78e5b9e0
Alex Allen <alex@koan.is>
2015-07-21 12:20:48 +0100

    More work on campaigs/create

--- src/MailChimpSharp.Core/Sections/Campaigns/CampaignCreateContentOptions.cs
+++ src/MailChimpSharp.Core/Sections/Campaigns/CampaignCreateContentOptions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MailChimpSharp.Core.Sections.Campaigns
 {
@@ -30,5 +32,210 @@ namespace MailChimpSharp.Core.Sections.Campaigns
         public string Text { get; internal set; }
 
         public string Url { get; internal set; }
+    }
+
+    public class CampaignCreateTypeOptions
+    {
+        public class AbSplitOptions
+        {
+            internal AbSplitOptions()
+            {
+            }
+
+            public enum PickWinnerOption
+            {
+                Opens, 
+                Clicks, 
+                Manual
+            }
+
+            public enum SplitTestType
+            {
+                Subject, 
+                FromName, 
+                Schedule
+            }
+
+            public string FromEmailA { get; internal set; }
+
+            public string FromEmailB { get; internal set; }
+
+            public string FromNameA { get; internal set; }
+
+            public string FromNameB { get; internal set; }
+
+            public PickWinnerOption PickWinner { get; internal set; }
+
+            public int SplitSize { get; internal set; }
+
+            public SplitTestType SplitTest { get; internal set; }
+
+            public string SubjectA { get; internal set; }
+
+            public string SubjectB { get; internal set; }
+
+            public int WaitTime { get; internal set; }
+
+            public int WaitUnits { get; internal set; }
+        }
+
+        public class AbSplitOptionsBuilder
+        {
+            public AbSplitOptionsBuilder()
+            {
+                Options = new AbSplitOptions();
+            }
+
+            private AbSplitOptions Options { get; set; }
+
+            public static implicit operator AbSplitOptions(AbSplitOptionsBuilder builder)
+            {
+                return builder.Options;
+            }
+
+            public AbSplitOptionsBuilder SplitOn(AbSplitOptions.SplitTestType splitTestType)
+            {
+                Options.SplitTest = splitTestType;
+                return this;
+            }
+
+            public AbSplitOptionsBuilder WonBy(AbSplitOptions.PickWinnerOption winner)
+            {
+                Options.PickWinner = winner;
+                return this;
+            }
+        }
+
+        public class RssOptions
+        {
+            internal RssOptions()
+            {
+                Schedule = RssSchedule.Daily;
+            }
+
+            public enum RssSchedule
+            {
+                Daily, 
+                Weekly, 
+                Monthly
+            }
+
+            public RssScheduleDays Days { get; internal set; }
+
+            public RssSchedule Schedule { get; internal set; }
+
+            public string ScheduleHour { get; internal set; }
+
+            public string ScheduleMonthDay { get; internal set; }
+
+            public DayOfWeek ScheduleWeekDay { get; internal set; }
+
+            public string Url { get; internal set; }
+
+            public class RssScheduleDays
+            {
+                public RssScheduleDays()
+                {
+                    Monday = true;
+                    Tuesday = true;
+                    Wednesday = true;
+                    Thursday = true;
+                    Friday = true;
+                    Saturday = true;
+                    Sunday = true;
+                }
+
+                public bool Friday { get; set; }
+
+                public bool Monday { get; set; }
+
+                public bool Saturday { get; set; }
+
+                public bool Sunday { get; set; }
+
+                public bool Thursday { get; set; }
+
+                public bool Tuesday { get; set; }
+
+                public bool Wednesday { get; set; }
+            }
+        }
+
+        public class RssOptionsBuilder
+        {
+            public RssOptionsBuilder()
+            {
+                Options = new RssOptions();
+            }
+
+            private RssOptions Options { get; set; }
+
+            public static implicit operator RssOptions(RssOptionsBuilder builder)
+            {
+                return builder.Options;
+            }
+
+            public RssOptionsBuilder At(int hour)
+            {
+                if (hour < 0 || hour > 24)
+                {
+                    throw new ArgumentException("Hour must be between 0 and 24");
+                }
+
+                Options.ScheduleHour = Convert.ToString(hour);
+                return this;
+            }
+
+            public RssOptionsBuilder FromUrl(string url)
+            {
+                Options.Url = url;
+                return this;
+            }
+
+            public RssOptionsBuilder On(DayOfWeek dayOfWeek)
+            {
+                Options.ScheduleWeekDay = dayOfWeek;
+                return this;
+            }
+
+            public RssOptionsBuilder On(params DayOfWeek[] daysOfWeek)
+            {
+                Options.Days = new RssOptions.RssScheduleDays
+                {
+                    Monday = daysOfWeek.Contains(DayOfWeek.Monday), 
+                    Tuesday = daysOfWeek.Contains(DayOfWeek.Tuesday), 
+                    Wednesday = daysOfWeek.Contains(DayOfWeek.Wednesday), 
+                    Thursday = daysOfWeek.Contains(DayOfWeek.Thursday), 
+                    Friday = daysOfWeek.Contains(DayOfWeek.Friday), 
+                    Saturday = daysOfWeek.Contains(DayOfWeek.Saturday), 
+                    Sunday = daysOfWeek.Contains(DayOfWeek.Sunday)
+                };
+
+                return this;
+            }
+
+            public RssOptionsBuilder OnDayOfMonth(int dayOfMonth)
+            {
+                if (dayOfMonth < 1 || dayOfMonth > 28)
+                {
+                    throw new ArgumentException("Day of month must be between 1 and 28");
+                }
+
+                Options.ScheduleMonthDay = Convert.ToString(dayOfMonth);
+                return this;
+            }
+
+            public RssOptionsBuilder OnLastDayOfMonth()
+            {
+                Options.ScheduleMonthDay = "last";
+                return this;
+            }
+
+            public RssOptionsBuilder UsingSchedule(RssOptions.RssSchedule schedule)
+            {
+                Options.Schedule = schedule;
+                return this;
+            }
+        }
     }
 }
--- src/MailChimpSharp.Core/Sections/Campaigns/CampaignCreateContentOptionsBuilder.cs
+++ src/MailChimpSharp.Core/Sections/Campaigns/CampaignCreateContentOptionsBuilder.cs
@@ -9,10 +9,21 @@
 
         private CampaignCreateContentOptions Options { get; set; }
 
-        public CampaignCreateContentOptionsBuilder WithHtml(string html)
+        public static implicit operator CampaignCreateContentOptions(CampaignCreateContentOptionsBuilder builder)
+        {
+            return builder.Options;
+        }
+
+        public CampaignCreateContentOptionsBuilder AsArchive(string archiveContent)
         {
             Options.Url = null;
-            Options.Html = html;
+            Options.Archive = archiveContent;
+            return this;
+        }
+
+        public CampaignCreateContentOptionsBuilder FromUrl(string url)
+        {
+            Options = new CampaignCreateContentOptions { Url = url };
             return this;
         }
 
@@ -23,29 +34,23 @@
             return this;
         }
 
+        public CampaignCreateContentOptionsBuilder WithArchiveType(CampaignCreateContentOptions.ArchiveTypeOption archiveType)
+        {
+            Options.ArchiveType = archiveType;
+            return this;
+        }
+
+        public CampaignCreateContentOptionsBuilder WithHtml(string html)
+        {
+            Options.Url = null;
+            Options.Html = html;
+            return this;
+        }
+
         public CampaignCreateContentOptionsBuilder WithText(string text)
         {
             Options.Url = null;
             Options.Text = text;
-            return this;
-        }
-
-        public CampaignCreateContentOptionsBuilder FromUrl(string url)
-        {
-            Options = new CampaignCreateContentOptions { Url = url };
-            return this;
-        }
-
-        public CampaignCreateContentOptionsBuilder AsArchive(string archiveContent)
-        {
-            Options.Url = null;
-            Options.Archive = archiveContent;
-            return this;
-        }
-
-        public CampaignCreateContentOptionsBuilder WithArchiveType(CampaignCreateContentOptions.ArchiveTypeOption archiveType)
-        {
-            Options.ArchiveType = archiveType;
             return this;
         }
     }
